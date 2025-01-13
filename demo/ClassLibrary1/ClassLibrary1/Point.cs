a50002985999c80ff7645ec5e569114fc876ede7
Nooooureldean <na3547697@gmail.com>
2025-01-13 19:00:28 +0200

    Add project files.

---
+++ demo/ClassLibrary1/ClassLibrary1/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public struct Point
+    {
+        public double CalculateDistance(int x1, int y1, int x2, int y2)
+        {
+           
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ demo/demo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using  ClassLibrary1;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p= new Point();
+            double distance = p.CalculateDistance(0, 0, 3, 4);
+            Console.WriteLine($"The distance is: {distance}");
+        }
+    }
+}
